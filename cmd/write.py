18ffd3501e95eab067217537177dbf8eb949348d
egersack <egersack@mit.edu>
2022-03-14 10:05:23 -0400

    commit

--- cmd/write.py
+++ cmd/write.py
@@ -10,3 +10,20 @@ cnx = mysql.connector.connect(user='root',
 #           YOUR CODE
 # ----------------------------------- 
 
+college = input('Enter college name: ')
+students = input('Enter student population: ')
+cursor = cnx.cursor()
+query = (f'INSERT INTO `Colleges` VALUES (NULL, "{college}", "{students}", NULL, NULL, NULL)')
+
+cursor.execute(query)
+
+query = ('SELECT * FROM colleges')
+cursor.execute(query)
+
+#print
+for row in cursor.fetchall():
+    print(row)
+
+cnx.commit()
+cursor.close()
+cnx.close()
