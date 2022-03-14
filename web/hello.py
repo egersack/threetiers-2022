4318ffa000122b0a844bf84957b7cd82192bbb39
egersack <egersack@mit.edu>
2022-03-14 10:16:29 -0400

    commit

--- web/hello.py
+++ web/hello.py
@@ -5,3 +5,15 @@ from flask import Flask
 #           YOUR CODE
 # ----------------------------------- 
 
+#create flask app
+app = Flask(__name__)
+
+#create root route
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+#start server
+if __name__ == '__main__':
+    app.run(debug = True, port = 3000)
+    
